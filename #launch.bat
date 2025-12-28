704dec755c2de520d2936d5e4c9d6d14c9a362ff
AlexD-76 <alex.diomin@roscovision.com>
2025-12-28 13:16:02 +0200

    Add device temperature; fix a file code page: Celsium character; add demo file and screenshot

---
+++ #launch.bat
@@ -0,0 +1,3 @@
+@echo off
+cls
+D:\projects\python\DeyeApi_rainmeter\.venv\Scripts\python main.py >out.txt
--- worker.py
+++ worker.py
@@ -12,7 +12,7 @@ class Worker:
     @staticmethod
     def _get_device_data_list_param(data_list, param_names: List) -> Dict:
         dict_values = {x['key']: x for x in data_list if x['key'] in param_names}
-        return {x: {'value': dict_values[x]['value'], 'value_str': f"{dict_values[x]['value']} {dict_values[x]['unit']}"} for x in param_names}
+        return {x: {'value': dict_values[x]['value'], 'value_str': f"{dict_values[x]['value'].replace('℃', '°C')} {dict_values[x]['unit'].replace('℃', '°C')}"} for x in param_names}
 
     def work(self, station='2508271645') -> Optional[Dict]:
         if self.api_client.auth():
@@ -23,7 +23,9 @@ class Worker:
                 'Station_id': station,
                 'Updated at': datetime.datetime.strftime(datetime.datetime.fromtimestamp(r['deviceDataList'][0]['collectionTime']), '%#d-%#m-%#y %#H:%M:%S')
             }
-            result.update(self._get_device_data_list_param(data_list=device_info, param_names=['TotalGridPower', 'BatteryVoltage']))
+            params = ['TotalGridPower', 'BatteryVoltage', 'DC Temperature', 'AC Temperature']
+            # params = ['TotalGridPower', 'BatteryVoltage']
+            result.update(self._get_device_data_list_param(data_list=device_info, param_names=params))
 
             if int(result['TotalGridPower']['value']) == 0:
                 result['Source'] = 'BATTERY'
